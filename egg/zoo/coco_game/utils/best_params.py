3b7e8363d54ad611d28ef54d1038444c0ac244eb
dizzi <nicofirst1@gmail.com>
2021-01-23 11:46:22 +0100

    add name change functinoality

--- egg/zoo/coco_game/utils/best_params.py
+++ egg/zoo/coco_game/utils/best_params.py
@@ -1,6 +1,9 @@
 import json
+import os
 from glob import glob
 
+import abbreviate
+import numpy as np
 from rich.console import Console
 from rich.progress import track
 from tensorboard.backend.event_processing import event_accumulator
@@ -42,41 +45,60 @@ def filter_params(res_dict):
 
     diffs = set(diffs)
     to_remove = set(
-        ['num_workers', "tensorboard_dir", "random_seed", "resume_training", "checkpoint_dir"])
+        ['num_workers', "tensorboard_dir", "random_seed", "resume_training", "checkpoint_dir", "log_dir", "n_epochs",
+         "checkpoint_freq", "use_rich_traceback", "log_dir_uid", "train_log_prob", "test_log_prob", "test_logging_step",
+         "train_logging_step", "data_root"])
     diffs -= to_remove
 
     for k, v in res_dict.items():
         res_dict[k] = {k2: v2 for k2, v2 in v.items() if k2 in diffs}
 
+    if len(res_dict) == 0:
+        return res_dict
+
+    all_keys = [list(x.keys()) for x in list(res_dict.values())]
+    m = np.argmin([len(x) for x in all_keys])
+    filtered_keys = all_keys[m]
+
+    for k, v in res_dict.items():
+        res_dict[k] = {k2: v2 for k2, v2 in v.items() if k2 in filtered_keys}
+
     return res_dict
 
 
-def find_accumulators(path2logs, tag="test/class_accuracy"):
+def find_accumulators(path2logs):
     logs = list(glob(path2logs + "/*"))
 
     res = {}
-    for idx in track(range(len(logs)), description="Processing events..."):
+    for idx in range(len(logs)):
         log = logs[idx]
-        event_path = log + "/tensorboard/events*"
-        try:
-            events = sorted(glob(event_path))[-1]
-        except IndexError:
+        if "random_signal" in log or "best" in log:
             continue
-        ac = get_accumulator(events, tag)
-
-        if ac is None:
-            continue
-        else:
-            ac *= 100
         params = get_params(log)
-        params[tag] = ac
-        params['log_dir_uid'] = params['log_dir_uid'].split("/")[-1]
-        res[ac] = params
+        uid = params['log_dir_uid'].split("/")[-1]
+
+        res[uid] = params
 
     return res
 
 
+
+def rename_logs(path, res_dict):
+    abbr = abbreviate.Abbreviate()
+    for k, v in res_dict.items():
+        v={abbr.abbreviate(k1, target_len=5):v1 for k1,v1 in v.items()}
+        new_name = str(v).replace("'", "") + "_" + k
+        new_name = os.path.join(path, new_name)
+        old_name = os.path.join(path, k)
+        os.rename(old_name, new_name)
+
+
 if __name__ == '__main__':
-    res = find_accumulators("Logs")
-    res = filter_params(res)
-    Console().log(sorted(res.items(), reverse=True))
+    main_path = "./AllLogs"
+    logs = list(glob(main_path + "/*"))
+
+    for lg in track(logs, description="Processing events..."):
+        res = find_accumulators(lg)
+        res = filter_params(res)
+        #Console().log(sorted(res.items(), reverse=True))
+        rename_logs(lg, res)
