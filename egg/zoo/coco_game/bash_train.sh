f3695e1c71c4363b9f694950a2e235fa1a4338f2
dizzi <nicofirst1@gmail.com>
2021-01-29 09:31:17 +0100

    empty bash

--- egg/zoo/coco_game/bash_train.sh
+++ egg/zoo/coco_game/bash_train.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 source ./args.sh
 
-all_args+=(
-  "--log_dir"
-  "$log_dir/Other"
 
-
-)
-
-python main.py "${all_args[@]}"  --sweep_file parameters.json #--sender_pretrain ./pretrain/Logs/Pretrain/b6636261 # --resume_training #
+python main.py "${all_args[@]}"  --sweep_file parameters.json # # --resume_training #
